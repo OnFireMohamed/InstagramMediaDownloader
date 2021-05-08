ed97a153957b3983535b9a07001d5992df9b0aba
OnFireMohamed <67835053+OnFireMohamed@users.noreply.github.com>
2021-05-08 17:26:51 +0400

    Update MohamedMatcher.java

--- MohamedMatcher.java
+++ MohamedMatcher.java
@@ -78,4 +78,49 @@ public class MohamedMatcher {
         }
         return val;
     }
+
+    String Unescape(String input) {
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        while (i < input.length()) {
+            char delimiter = input.charAt(i); i++; 
+
+            if(delimiter == '\\' && i < input.length()) {
+
+                
+                char ch = input.charAt(i); i++;
+
+                if(ch == '\\' || ch == '/' || ch == '"' || ch == '\'') {
+                    builder.append(ch);
+                }
+                else if(ch == 'n') builder.append('\n');
+                else if(ch == 'r') builder.append('\r');
+                else if(ch == 't') builder.append('\t');
+                else if(ch == 'b') builder.append('\b');
+                else if(ch == 'f') builder.append('\f');
+                else if(ch == 'u') {
+
+                    StringBuilder hex = new StringBuilder();
+
+                    // expect 4 digits
+                    if (i+4 > input.length()) {
+                       return "";
+                    }
+                    for (char x : input.substring(i, i + 4).toCharArray()) {
+                        if(!Character.isLetterOrDigit(x)) {
+                            return "";
+                        }
+                        hex.append(Character.toLowerCase(x));
+                    }
+                    i+=4; // consume those four digits.
+
+                    int code = Integer.parseInt(hex.toString(), 16);
+                    builder.append((char) code);
+                }
+            } else {
+                builder.append(delimiter);
+            }
+        }
+        return builder.toString();
+    }
 }
