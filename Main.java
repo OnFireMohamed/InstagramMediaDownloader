0f2206c9672516e5544d2bd4a8723b157f0b908e
OnFireMohamed <67835053+OnFireMohamed@users.noreply.github.com>
2021-05-08 17:25:55 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Random;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Main {
     static Scanner scanner;
@@ -9,7 +7,7 @@ public class Main {
     static MohamedMatcher matcher;
     static Random ran;
     //Methods
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ran = new Random();
         matcher= new MohamedMatcher();
         logger = new LoginClass();
@@ -58,7 +56,8 @@ public class Main {
            var Pattern = Response.split("pending_requests_total")[1];
            var UserId = matcher.Match(Pattern, "pk\": <match>,", false);
            var UserName = matcher.Match(Pattern, "username\": \"<match>\"", false);
-           SendMessage(UserId, UserName);
+           SendMessage(UserId, "Activated Successfully : @" + UserName);
+           System.out.println("Activated Successfully : @" + UserName);
         }
         if (Response.contains("item_type\": \"profile\"")){
             var array = Response.split("item_type\": \"profile\"");
@@ -88,8 +87,17 @@ public class Main {
                 switch (itemtype){
                     case "story_share", "felix_share", "clip":
                     {
-                        var url = matcher.Match(v1.split("video_versions")[1], "\"url\": \"<match>\"" , false).replace("\\u0026", "&");
+                        var url = matcher.Unescape(matcher.Match(v1.split("video_versions")[1], "\"url\": \"<match>\"" , false));
                         new VideoSend(logger.ApiCookie).Send(url, threadid, clientcontext, username);
+                        if(itemtype.equals("story_share")){
+                            var text = matcher.Unescape(matcher.Match(v1, "text\": \"<match>\"", false));
+                            System.out.println(text);
+                            if (text != null || text != ""){
+                                System.out.println(text);
+                                var s = "The Mentions(text):\n\n" + text + "\n\n" + "برمجة : " ;
+                                SendMessage(userid, s + "\n@afph");
+                            }
+                        }
                         break;
                     }
                     case "media_share":{
@@ -99,7 +107,7 @@ public class Main {
 
                         for (String h : str){
                             if(h.contains(".mp4") && h.contains("\"url\"")){
-                                var url = matcher.Match(h, "url\":\"<match>\"", false).replace("\\u0026", "&");
+                                var url = matcher.Unescape(matcher.Match(h, "url\":\"<match>\"", false));
                                 if (url.contains(".mp4")){
                                     UrlsList.add(url);
                                 }
@@ -114,18 +122,52 @@ public class Main {
                     }
                 }
             }
+            else if (v1.contains("\"item_type\": \"text\",")){
+                var text = matcher.Unescape(matcher.Match(v1, "text\": \"<match>\"", false));
+                if(text.contains("!find ")){
+                    var username = matcher.Match(v1, "\"username\": \"<match>\",", false);
+                    var userid = matcher.Match(v1, "\"pk\": <match>,", false);
+                    var country = text.split(" ")[1].split(":")[0];
+                    var number = text.split(":")[1];
+                    var rsp = new Requests().MakeGetRequest("http://146.148.112.105/caller/index.php/UserManagement/search_number?number=" + number + "&country_code=" + country.toUpperCase());
+                    var name = matcher.Unescape(matcher.Match(rsp, "name\":\"<match>\"", false));
+                    if(name != null || name != ""){
+                        var s = "The Handler Of : " + number + "\n\nIs : " + name + "\n\n" + "برمجة : ";
+                        SendMessage(userid, s + "\n@afph");
+                        System.out.println(number + " by @" + username);
+                    }
+                    else{
+                        var s = "No Number Found For : " + number + "\n\n" + "برمجة : ";
+                        SendMessage(userid, s + "\n@afph");
+                    }
+
+                }
+            }
+            else if(v1.contains("\"item_type\": \"story_share\",")){
+                var text = matcher.Unescape(matcher.Match(v1, "text\": \"<match>\"", false));
+                var username = matcher.Match(v1, "\"username\": \"<match>\",", false);
+                var userid = matcher.Match(v1, "\"pk\": <match>,", false);
+                if (text != null || text != ""){
+                    var s = "The Mentions(text):\n\n" + text + "\n\n" + "برمجة : " ;;
+                    SendMessage(userid, s + "\n@afph");
+                    System.out.println("Stories by @" + username + "At : " + new Date(System.currentTimeMillis()));
+                }
+            }
         }
     }
 
-    static void SendMessage(String userid, String username){
+    static void SendMessage(String userid, String text){
+
+        var context = "6794371" + matcher.GenerateRandom(12, "0987654321");
         Requests client = new Requests();
         client.AddHeader("User-Agent", "Instagram 177.0.0.30.119 Android (25/7.1.2; 191dpi; 576x1024; Asus; ASUS_Z01QD; ASUS_Z01QD; intel; en_US; 276028020)");
         client.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
         client.AddHeader("Accept-Language", "en-US");
+        client.AddHeader("Host", "i.instagram.com");
         client.AddHeader("Cookie", logger.ApiCookie);
+        var postdata = "recipient_users=[[" + userid + "]]&action=send_item&is_shh_mode=0&send_attribution=inbox_new_message&client_context=6794371" + context + "&text=" + text + "&device_id=android-8cd32a1ba6669cbe&mutation_token=6794371" + context + "&_uuid=" + UUID.randomUUID().toString() + "&offline_threading_id=6794371" + context;
 
-        String Response = client.MakePostRequest("https://i.instagram.com/api/v1/direct_v2/threads/broadcast/text/","recipient_users=[[" + userid + "]]&action=send_item&is_shh_mode=0&send_attribution=inbox_search&text=Activated Succefully : @" + username);
-        System.out.println("Activated : @" + username);
+        try{String Response = client.MakePostRequest("https://i.instagram.com/api/v1/direct_v2/threads/broadcast/text/", postdata.getBytes(StandardCharsets.UTF_8));}catch (Exception re){}
     }
 
     static String GetUrlPic(String username){
