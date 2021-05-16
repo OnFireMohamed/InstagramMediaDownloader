22fd1ae22fc49feaf2b2d3c9b2f5bab7211c6a63
OnFireMohamed <67835053+OnFireMohamed@users.noreply.github.com>
2021-05-16 21:32:19 +0400

    Add files via upload

---
+++ LoginClass.java
@@ -0,0 +1,186 @@
+import javax.swing.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.UUID;
+
+public class LoginClass {
+
+    MohamedMatcher matcher = new MohamedMatcher();
+    String Webcookie, ApiCookie;
+    final String UserAgent = "Instagram 177.0.0.30.119 Android (25/7.1.2; 191dpi; 576x1024; Asus; ASUS_Z01QD; ASUS_Z01QD; intel; en_US; 276028020)";
+    LoginClass(){
+        Requests client = new Requests();
+        client.AddHeader("User-Agent", UserAgent);
+        var Response = client.MakeGetRequest("https://i.instagram.com/api/v1/si/fetch_headers/?challenge_type=signup");
+        var ResponseHeaders = client.GetResponseHeader("set-cookie");
+        ApiCookie =  "csrftoken=" + ResponseHeaders.split("csrftoken=")[1].split(";")[0] + "; mid=" + ResponseHeaders.split("mid=")[1].split(";")[0];
+    }
+
+
+    boolean WebLogin(String user, String pass){
+        Requests client = new Requests();
+        client.AddHeader("Host", "www.instagram.com");
+        client.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
+        client.AddHeader("Accept", "*/*");
+        client.AddHeader("Accept-Language", "en-US,en;q=0.5");
+        client.AddHeader("X-CSRFToken", "NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI");
+        client.AddHeader("X-Instagram-AJAX", "2f5a8c09a5f5");
+        client.AddHeader("X-IG-App-ID", "936619743392459");
+        client.AddHeader("X-IG-WWW-Claim", "0");
+        client.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+        client.AddHeader("X-Requested-With", "XMLHttpRequest");
+        client.AddHeader("Connection", "keep-alive");
+        client.AddHeader("Cookie", "csrftoken=NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI; mid=YIDMOQALAAEWa_CELZYuPHy6DUJl; ig_did=5D03B3E6-1421-4D0B-BF02-950A762D55C0; ig_nrcb=1");
+
+        String Response = client.MakePostRequest("https://www.instagram.com/accounts/login/ajax/", "username=" + user + "&enc_password=#PWD_INSTAGRAM_BROWSER:0:1619193893:" + pass + "&queryParams={}&optIntoOneTap=false");
+        if (Response.contains("authenticated\":true")){
+            String SetCookie = client.GetResponseHeader("set-cookie");
+            Webcookie = "csrftoken=NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI; mid=YIDMOQALAAEWa_CELZYuPHy6DUJl; ig_did=5D03B3E6-1421-4D0B-BF02-950A762D55C0; ig_nrcb=1; sessionid=" + SetCookie.split("sessionid=")[1].split(";")[0];
+            return true;
+        }
+
+
+
+        else if (Response.contains("checkpoint_required")){
+            String path = matcher.Match(Response, "checkpoint_url\":\"<match>\"", false);
+            return WebPostChoice(path);
+        }
+
+        else
+            return false;
+    }
+    boolean WebPostChoice(String path){
+        try {
+            String Choice = JOptionPane.showInputDialog("Enter Your Choice :\n0 to send to Phone, 1 to Email");
+            while (Integer.parseInt(Choice) > 1 || Integer.parseInt(Choice) < 0){
+                Choice = JOptionPane.showInputDialog("Choose 0 or 1 !");
+            }
+            Requests client = new Requests();
+            client.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
+            client.AddHeader("Accept", "*/*");
+            client.AddHeader("Accept-Language", "en-US,en;q=0.5");
+            client.AddHeader("X-CSRFToken", "NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI");
+            client.AddHeader("X-Instagram-AJAX", "2f5a8c09a5f5");
+            client.AddHeader("X-IG-App-ID", "936619743392459");
+            client.AddHeader("X-IG-WWW-Claim", "0");
+            client.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            client.AddHeader("X-Requested-With", "XMLHttpRequest");
+            client.AddHeader("Cookie", "csrftoken=NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI; mid=YIDMOQALAAEWa_CELZYuPHy6DUJl; ig_did=5D03B3E6-1421-4D0B-BF02-950A762D55C0; ig_nrcb=1");
+
+            String Response = client.MakePostRequest("https://www.instagram.com" + path, "choice=" + Choice);
+            if(Response.contains("code we sent")){
+                String SentWhere = matcher.Match(Response,"contact_point\":\"<match>\"", false);
+                return PostwebCode(path, SentWhere);
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+        return false;
+    }
+
+    boolean PostwebCode(String path, String SentWhere){
+        try {
+            String code = JOptionPane.showInputDialog("Enter Code Which Sent to " + SentWhere);
+            Requests client = new Requests();
+            client.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
+            client.AddHeader("Accept", "*/*");
+            client.AddHeader("Accept-Language", "en-US,en;q=0.5");
+            client.AddHeader("X-CSRFToken", "NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI");
+            client.AddHeader("X-Instagram-AJAX", "2f5a8c09a5f5");
+            client.AddHeader("X-IG-App-ID", "936619743392459");
+            client.AddHeader("X-IG-WWW-Claim", "0");
+            client.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            client.AddHeader("X-Requested-With", "XMLHttpRequest");
+            client.AddHeader("Cookie", "csrftoken=NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI; mid=YIDMOQALAAEWa_CELZYuPHy6DUJl; ig_did=5D03B3E6-1421-4D0B-BF02-950A762D55C0; ig_nrcb=1");
+
+            String Response = client.MakePostRequest("https://www.instagram.com" + path, "security_code=" + code);
+            if (Response.contains("status\":\"ok")){
+                String SetCookie = client.GetResponseHeader("set-cookie");
+                Webcookie = "csrftoken=NN29ClUMPblTvLb57PnGVoiRQ4bmF7hI; mid=YIDMOQALAAEWa_CELZYuPHy6DUJl; ig_did=5D03B3E6-1421-4D0B-BF02-950A762D55C0; ig_nrcb=1; sessionid=" + SetCookie.split("sessionid=")[1].split(";")[0];
+                return true;
+            }
+            else if (Response.contains("check the code we sent you and try again")){
+                return PostwebCode(path, SentWhere);
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+        return false;
+    }
+    protected final String s = new String(Base64.getDecoder().decode("CgrYqNix2YXYrNipIDogCkBhZnBo"), StandardCharsets.UTF_8);
+
+    // App Login :
+
+    boolean AppLogin(String User, String Pass){
+        Requests client =  new Requests();
+        client.AddHeader("User-Agent", UserAgent);
+        client.AddHeader("Content-Type: application/x-www-form-urlencoded");
+        client.AddHeader("Accept-Language", "en-US,en;q=0.9");
+        client.AddHeader("Cookie", ApiCookie);
+        String Response = client.MakePostRequest("https://i.instagram.com/api/v1/accounts/login/", "signed_body=SIGNATURE.{\"jazoest\":\"22485\",\"country_codes\":\"[{\\\"country_code\\\":\\\"1\\\",\\\"source\\\":[\\\"default\\\"]},{\\\"country_code\\\":\\\"968\\\",\\\"source\\\":[\\\"sim\\\"]}]\",\"phone_id\":\"" + UUID.randomUUID().toString() + "\",\"enc_password\":\"#PWD_INSTAGRAM:0:1614329314:" + Pass + "\",\"username\":\"" + User + "\",\"adid\":\"" + UUID.randomUUID().toString() + "\",\"guid\":\"" + UUID.randomUUID().toString() + "\",\"device_id\":\"" + UUID.randomUUID().toString() + "\",\"google_tokens\":\"[]\",\"login_attempt_count\":\"0\"}");
+
+        if(Response.contains("bad_password"))
+            return false;
+        else if(Response.contains("logged_in_user"))
+        {
+            InstagramSetCookie(client.GetResponseHeader("set-cookie"));
+            return true;
+        }
+
+        else if(Response.contains("challenge_required")){
+            String path = matcher.Match(Response,"api_path\":\"<match>\"",false);
+            return PostAppChoice(path);
+        }
+        return false;
+    }
+
+    boolean PostAppChoice(String path){
+        String Choice = JOptionPane.showInputDialog("Enter Your Choice :\n0 to send to Phone, 1 to Email");
+        while (Integer.parseInt(Choice) > 1 || Integer.parseInt(Choice) < 0){
+            Choice = JOptionPane.showInputDialog("Choose 0 or 1 !");
+        }
+        Requests client =  new Requests();
+        client.AddHeader("User-Agent", UserAgent);
+        client.AddHeader("Content-Type: application/x-www-form-urlencoded");
+        client.AddHeader("Accept-Language", "en-US,en;q=0.9");
+        client.AddHeader("Cookie", ApiCookie);
+        String Response = client.MakePostRequest("https://i.instagram.com/api/v1" + path,"choice=" + Choice);
+        if (Response.contains("Select a valid choice")){
+            return PostAppChoice(path);
+        }
+        else if(Response.contains("security_code")){
+            String SentWhere = matcher.Match(Response, "contact_point\":\"<match>\"", false);
+            return PostCodeApp(path, SentWhere);
+        }
+        return false;
+    }
+
+    boolean PostCodeApp(String path, String SentWhere){
+        String Code = JOptionPane.showInputDialog("Enter Code which sent to : " + SentWhere);
+        Requests client =  new Requests();
+        client.AddHeader("User-Agent", UserAgent);
+        client.AddHeader("Content-Type: application/x-www-form-urlencoded");
+        client.AddHeader("Accept-Language", "en-US,en;q=0.9");
+        client.AddHeader("Cookie", ApiCookie);
+        String Response = client.MakePostRequest("https://i.instagram.com/api/v1" + path,"security_code=" + Code);
+        if (Response.contains("logged_in_user")){
+            InstagramSetCookie(client.GetResponseHeader("set-cookie"));
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private void InstagramSetCookie(String v){
+        ApiCookie = matcher.Match(v, "csrftoken=<match>;", true) + " " + matcher.Match(v, "rur=<match>;", true) + " " + matcher.Match(v, "mid=<match>;", true) + " " + matcher.Match(v, "ds_user_id=<match>;", true) + " sessionid=" + matcher.Match(v, "sessionid=<match>;", false);
+        if(ApiCookie.contains("  "))
+            ApiCookie = ApiCookie.replace("  ", " ");
+    }
+
+}
---
+++ MainClass.java
@@ -0,0 +1,239 @@
+import javax.swing.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class MainClass {
+    static Scanner scanner;
+    static LoginClass logger;
+    static MohamedMatcher matcher;
+    static Random ran;
+
+
+
+    //Methods
+    public static void main(String[] args) {
+        ran = new Random();
+        matcher= new MohamedMatcher();
+        logger = new LoginClass();
+        scanner = new Scanner(System.in);
+
+        if (logger.ApiCookie != null || logger.ApiCookie != ""){
+
+            System.out.print("Enter Username : ");
+            var user = scanner.nextLine();
+            System.out.print("Enter Password : ");
+            var pass = scanner.nextLine();
+            if(logger.AppLogin(user, pass) && logger.WebLogin(user, pass)){
+                System.out.println("[ + ] Logged In");
+                while (true){
+                    CheckDirect();
+                    try{Thread.sleep(RandomSleep());}catch (Exception e){}
+
+                }
+            }
+            else{
+                System.out.println("[ + ] Error in login");
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+    static int RandomSleep(){
+        int num = ran.nextInt(120000);
+
+        if(num <= 60000){
+            return RandomSleep();
+        }
+        else
+            return num;
+    }
+    static void CheckDirect(){
+        Requests client = new Requests();
+        client.AddHeader("User-Agent", "Instagram 177.0.0.30.119 Android (25/7.1.2; 191dpi; 576x1024; Asus; ASUS_Z01QD; ASUS_Z01QD; intel; en_US; 276028020)");
+        client.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        client.AddHeader("Accept-Language", "en-US");
+        client.AddHeader("Cookie", logger.ApiCookie);
+        String Response = client.MakeGetRequest("https://i.instagram.com/api/v1/direct_v2/inbox/?visual_message_return_type=unseen&persistentBadging=true&use_unified_inbox=true");
+        try{
+            if(! Response.contains("pending_requests_total\": 0,"))
+            {
+                var Pattern = Response.split("pending_requests_total")[1];
+                var UserId = matcher.Match(Pattern, "pk\": <match>,", false);
+                var UserName = matcher.Match(Pattern, "username\": \"<match>\"", false);
+                SendMessage(UserId, "Activated Successfully : @" + UserName);
+                System.out.println("Activated Successfully : @" + UserName);
+            }
+            if (! Response.contains("username")){
+                JOptionPane.showMessageDialog(null, "[ + ] Error At : " + new Date(System.currentTimeMillis()));
+            }
+
+            String[] v = Response.split("thread_id");
+            for(String v1 : v){
+                if(v1.contains("video_versions")){
+                    var threadid = matcher.Match(v1, "\": \"<match>\",", false);
+                    var username = matcher.Match(v1, "\"username\": \"<match>\",", false);
+                    var userid = matcher.Match(v1, "\"pk\": <match>,", false);
+                    var itemtype = matcher.Match(v1, "\"item_type\": \"<match>\",", false);
+                    var mediaid = matcher.Match(v1, "media_id\": <match>,", false);
+                    var timestamp = matcher.Match(v1, "timestamp\": <match>," , false);
+                    var clientcontext = matcher.Match(v1, "client_context\": \"<match>\"", false);
+                    switch (itemtype){
+                        case "story_share", "felix_share", "clip":
+                        {
+                            var url = matcher.Unescape(matcher.Match(v1.split("video_versions")[1], "\"url\": \"<match>\"" , false));
+                            new VideoSend(logger.ApiCookie).Send(url, threadid, clientcontext, username);
+                            if(itemtype.equals("story_share")){
+                                var text = matcher.Unescape(matcher.Match(v1, "text\": \"<match>\"", false));
+                                if (text != "" && text.length() > 1){
+                                    var s = "The Mentions(text):\n\n" + text ;
+                                    SendMessage(userid, s);
+                                }
+                            }
+                            break;
+                        }
+                        case "media_share":{
+                            var Json = GetMediaJson(mediaid);
+                            var str = Json.split("video_versions");
+                            ArrayList<String> UrlsList = new ArrayList<String>();
+
+                            for (String h : str){
+                                if(h.contains(".mp4") && h.contains("\"url\"")){
+                                    var url = matcher.Unescape(matcher.Match(h, "url\":\"<match>\"", false));
+                                    if (url.contains(".mp4")){
+                                        UrlsList.add(url);
+                                    }
+                                }
+                            }
+                            for (String url : UrlsList) {
+                                new VideoSend(logger.ApiCookie).Send(url, threadid, clientcontext, username);
+                                try {Thread.sleep(2000);}catch(Exception e){}
+                            }
+
+                            break;
+                        }
+                    }
+                }
+                else if (v1.contains("\"item_type\": \"text\",")){
+                    var text = matcher.Unescape(matcher.Match(v1, "text\": \"<match>\"", false));
+                    if(text.contains("!find ")){
+                        var username = matcher.Match(v1, "\"username\": \"<match>\",", false);
+                        var userid = matcher.Match(v1, "\"pk\": <match>,", false);
+                        var country = text.split(" ")[1].split(":")[0];
+                        var number = text.split(":")[1];
+                        var rsp = new Requests().MakeGetRequest("http://146.148.112.105/caller/index.php/UserManagement/search_number?number=" + number + "&country_code=" + country.toUpperCase());
+
+                        if(rsp.contains("name")){
+                            var name = matcher.Unescape(matcher.Match(rsp, "name\":\"<match>\"", false));
+                            if(name != null || name != ""){
+                                var s = "The Handler Of : " + number + "\n\nIs : " + name;
+                                SendMessage(userid, s);
+                                System.out.println(number + " by @" + username);
+                            }
+                        }
+
+                        else{
+                            var s = "No Number Found For : " + number;
+                            SendMessage(userid, s );
+                        }
+
+                    }
+                }
+                else if(v1.contains("\"item_type\": \"story_share\",")){
+                    var text = matcher.Unescape(matcher.Match(v1, "text\": \"<match>\"", false));
+                    var username = matcher.Match(v1, "\"username\": \"<match>\",", false);
+                    var userid = matcher.Match(v1, "\"pk\": <match>,", false);
+                    if (text != "" && text.length() > 1){
+                        var s = "The Mentions(text):\n\n" + text  ;
+                        SendMessage(userid, s );
+                        System.out.println("Stories by @" + username + "At : " + new Date(System.currentTimeMillis()));
+                    }
+                }
+                else if(v1.contains("\"item_type\": \"profile\",")){
+                    var threadid = matcher.Match(v1, "\": \"<match>\",", false);
+                    var SenderUsername = matcher.Match(v1, "\"username\": \"<match>\",", false);
+                    var SenderUserId = matcher.Match(v1, "\"pk\": <match>,", false);
+                    var array = v1.split("\"item_type\": \"profile\",");
+                    var username = matcher.Match(array[1], "\"username\": \"<match>\",", false);
+                    var userid = matcher.Match(array[1], "\"pk\": <match>,", false);
+                    var AccountJson = GetaccountJson(username);
+                    var url = matcher.Match(AccountJson, "profile_pic_url_hd\":\"<match>\"", false);
+
+
+
+                     if(AccountJson.contains("is_private\":true") && AccountJson.contains("followed_by_viewer\":false") && AccountJson.contains("requested_by_viewer\":false")){
+
+                         if (Follow(userid) == true){
+                             System.out.println("Follow Requested : @" + username);
+                             SendMessage(SenderUserId, "Follow Requested : @" + username);
+                         }
+
+                         else{
+                             System.out.println("Can't Request Follow : @" + username);
+                             SendMessage(SenderUserId, "Can't Request Follow : @" + username);
+                         }
+
+
+                     }
+
+
+
+                if(url != null || url != ""){
+                    new PhotoSend(logger.ApiCookie).System(url, username, SenderUsername, threadid);
+                }
+                }
+            }
+        }
+        catch (Exception e){
+
+        }
+
+    }
+
+    static void SendMessage(String userid, String text){
+        if(!text.contains("Activated Successfully")) text += logger.s;
+        var context = "6794371" + matcher.GenerateRandom(12, "0987654321");
+        Requests client = new Requests();
+        client.AddHeader("User-Agent", "Instagram 177.0.0.30.119 Android (25/7.1.2; 191dpi; 576x1024; Asus; ASUS_Z01QD; ASUS_Z01QD; intel; en_US; 276028020)");
+        client.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        client.AddHeader("Accept-Language", "en-US");
+        client.AddHeader("Host", "i.instagram.com");
+        client.AddHeader("Cookie", logger.ApiCookie);
+        var postdata = "recipient_users=[[" + userid + "]]&action=send_item&is_shh_mode=0&send_attribution=inbox_new_message&client_context=6794371" + context + "&text=" + text + "&device_id=android-8cd32a1ba6669cbe&mutation_token=6794371" + context + "&_uuid=" + UUID.randomUUID().toString() + "&offline_threading_id=6794371" + context;
+
+        try{String Response = client.MakePostRequest("https://i.instagram.com/api/v1/direct_v2/threads/broadcast/text/", postdata.getBytes(StandardCharsets.UTF_8));}catch (Exception re){}
+    }
+
+    static String GetaccountJson(String username){
+
+        Requests client = new Requests();
+        client.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+        client.AddHeader("accept-language", "en-US,en;q=0.9");
+        client.AddHeader("cookie", logger.Webcookie);
+        client.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36");
+        String Response = client.MakeGetRequest("https://www.instagram.com/" + username + "/?__a=1");
+        return Response;
+    }
+    static boolean Follow(String UserId){
+        Requests client = new Requests();
+        client.AddHeader("User-Agent", "Instagram 177.0.0.30.119 Android (25/7.1.2; 191dpi; 576x1024; Asus; ASUS_Z01QD; ASUS_Z01QD; intel; en_US; 276028020)");
+        client.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        client.AddHeader("Accept-Language", "en-US");
+        client.AddHeader("Cookie", logger.ApiCookie);
+        String Response = client.MakePostRequest(String.format("https://i.instagram.com/api/v1/friendships/create/%s/", UserId), "");
+        if (Response.contains("outgoing_request\":true"))
+            return true;
+        else
+            return false;
+    }
+    static String GetMediaJson(String MediaId){
+        Requests client = new Requests();
+        client.AddHeader("User-Agent", "Instagram 177.0.0.30.119 Android (25/7.1.2; 191dpi; 576x1024; Asus; ASUS_Z01QD; ASUS_Z01QD; intel; en_US; 276028020)");
+        client.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        client.AddHeader("Accept-Language", "en-US");
+        client.AddHeader("Cookie", logger.ApiCookie);
+        String Response = client.MakeGetRequest("https://i.instagram.com/api/v1/media/" + MediaId + "/info/");
+        return Response;
+    }
+
+}
